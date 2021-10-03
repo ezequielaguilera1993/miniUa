d71b32e959fc8487d3959f0f236bd65ae757c728
EzequielAguilera <ezequielaguilera1993@gmail.com>
2021-10-03 06:07:39 -0300

    simple GET server

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,48 @@
 package com.company;
 
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("ASd");
+    public static void main(String[] args) throws IOException {
+        // write your code here
+        int port = 8081;
+
+        ServerSocket serverSocket = new ServerSocket(port);
+
+        System.out.println("Serer running on: "+port);
+
+        while (true) {
+            Socket client=serverSocket.accept();
+            InputStreamReader isr = new InputStreamReader(client.getInputStream());
+            BufferedReader br = new BufferedReader(isr);
+
+            StringBuilder request = new StringBuilder();
+
+            String line;
+            line = br.readLine();
+            while (!line.isBlank()) {
+                request.append(line+"\r\n");
+                line = br.readLine();
+            }
+            System.out.println("message");
+            String firstLine= request.toString().split("\n")[0];
+
+            String verbe = firstLine.split(" ")[0];
+
+            System.out.println(verbe);
+
+            OutputStream clientOuput = client.getOutputStream();
+            clientOuput.write("HTTP/1.1 200 OK \r\n".getBytes());
+            clientOuput.write("\r\n".getBytes());
+            clientOuput.write("Welcome babe".getBytes());
+            clientOuput.flush();
+            clientOuput.close();
+        }
+
 
     }
 }

93d5613a9ef4beee097577eca87cefdcc94f81c9
EzequielAguilera <ezequielaguilera1993@gmail.com>
2021-10-03 08:13:24 -0300

    simple go hello world server

--- goHelloWorldServer.go
+++ goHelloWorldServer.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-"fmt"
-"net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
+
 http.HandleFunc("/", SimpleServer)
+http.ListenAndServe(":8000", nil)
+
 }
 
 func SimpleServer(w http.ResponseWriter, r *http.Request){
-fmt.Fprint(w, "Hello %s", r.URL.Path)
+fmt.Fprint(w, "<h1>Hello Mundo</h1>")
 }
